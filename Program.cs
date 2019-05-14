76d72b463e3af8ce48087a672b38f24dfc0c68af
chaos09x <34987193+chaos09x@users.noreply.github.com>
2019-05-13 20:01:31 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Threading;
+using Acumen.Bank.Account;
+
+namespace Acumen.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acumen Bank!");
+		    Console.WriteLine();
+
+		    CheckingAccount michaelsAccount = new CheckingAccount("Michael", 5000);
+		    CheckingAccount gobsAccount = new CheckingAccount("Gob", 2000);
+
+		    Console.WriteLine("Open Accounts:");
+		    Console.WriteLine();
+		    PrintAccountDetails(michaelsAccount);
+		    Console.WriteLine();
+		    PrintAccountDetails(gobsAccount);
+
+		    Console.WriteLine();
+		    Console.WriteLine("Making transfer of $1000.00...");
+            try
+            {
+                Thread.Sleep(500);
+            }
+            catch (ThreadInterruptedException)
+            {
+                return;
+            }
+            Console.WriteLine();
+		    michaelsAccount.Transfer(gobsAccount, 1000, michaelsAccount);
+
+		    Console.WriteLine("Updated Account Details:");
+		    Console.WriteLine();
+		    PrintAccountDetails(michaelsAccount);
+		    Console.WriteLine();
+		    PrintAccountDetails(gobsAccount);
+
+            //Console.ReadLine();
+            // sample code for savings account implementation
+            // Initialize new savings account with initial balance of $30,000 and 0.89% interest
+            SavingsAccount acesSavingsAccount = new SavingsAccount("Ace", 30000, .0089);
+            // Initialize new savings account with initial balance of $10,000 and .56% interest
+            SavingsAccount garysSavingsAccount = new SavingsAccount("Gary", 10000, .0056);
+            
+            acesSavingsAccount.TransferSave2Save(garysSavingsAccount, 5000, acesSavingsAccount);
+            Console.WriteLine();
+            Console.WriteLine("Making transfer of $5000.00...");
+            try
+            {
+                Thread.Sleep(500);
+            }
+            catch (ThreadInterruptedException)
+            {
+                return;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Updated Account Details:");
+            Console.WriteLine("");
+            PrintAccountDetails2(acesSavingsAccount);
+            Console.WriteLine();
+            PrintAccountDetails2(garysSavingsAccount);
+            // apply 2 years of interest to the savings accounts
+            Console.Write("Please enter years for interest accumulation: "); 
+            acesSavingsAccount.Interest(int.Parse(Console.ReadLine()));
+            Console.WriteLine();
+            garysSavingsAccount.Interest(2);
+
+            Console.WriteLine("Updated Account Details:");
+            Console.WriteLine("");
+            PrintAccountDetails2(acesSavingsAccount);
+            Console.WriteLine();
+            PrintAccountDetails2(garysSavingsAccount);
+
+            Console.ReadLine();
+        }
+
+	    private static void PrintAccountDetails(CheckingAccount account) {
+		    Console.WriteLine("Account for {0}:\r\n", account.OwnerName);
+            Console.WriteLine("Balance: {0:C2}\r\n", account.Balance);
+	    }
+        private static void PrintAccountDetails2(SavingsAccount account){
+            Console.WriteLine("Account for {0}:\r\n", account.OwnerName);
+            Console.WriteLine("Balance: {0:C2}\r\n", account.Balance);
+        }
+    }
+}
---
+++ SavingsAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Acumen.Bank.Account
+{
+    public class SavingsAccount
+    {
+        public string OwnerName { get; private set; }
+        public double Balance { get; private set; }
+        public double Rate { get; private set; }
+
+        public SavingsAccount(string ownerName, double balance, double rate)
+        {
+            this.OwnerName = ownerName;
+            this.Balance = balance;
+            this.Rate = rate;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Cannot deposit a negative amount");
+            }
+            this.Balance += amount;
+        }
+
+        public void TransferSave2Save(SavingsAccount destinationAccount, double amount, SavingsAccount sourceAccount)
+        {
+            if (amount > sourceAccount.Balance)
+            {
+                throw new ArgumentException("Source account too low");
+            }
+            else
+            {
+                destinationAccount.Deposit(amount);
+                sourceAccount.Balance -= amount;
+            }
+        }
+
+        public void Interest(double years)
+        {
+            // P1 = P(1+(r/n)^nt where P1 = the new principal sum
+            // P = the original payment, r = the nominal annual interest rate
+            // n = the compounding frequency, t = the overall length of time(usually in years)
+            double P = this.Balance; // user investment + current bal.
+            double r = this.Rate; // personalized rate for account
+            double n = 1; // this will only compound once per year
+            double t = years; // this is determined by the number of years the account is open
+            double P1 = P * Math.Pow((1 + (r / n)), (n * t));
+            double gain = Math.Round(P1 - P, 2);
+            this.Balance += gain;
+            Console.WriteLine("Interest Gained for " + this.OwnerName + " over " +
+                years + " years " + gain.ToString());
+            Console.WriteLine();
+        }
+    }
+}
